b61abc5a2db445435d6cdcc0467d137ad29f678c
Shark@DESKTOP-E8806R4 <sharknado8575@gmail.com>
2022-06-06 22:45:32 -0400

    Faster than before

--- echo-client.py
+++ echo-client.py
@@ -1,19 +1,31 @@
 # echo-client.py
 import time
 import socket
-import torchvision.transforms as transforms
+import cv2
 
 BUFFER_SIZE = 4096
 #HOST = "192.168.1.154"  # The server's hostname or IP address
+#HOST = "192.168.1.153" #Raspberry PI
 HOST = "127.0.0.1" #Localhost if needed 
 PORT = 4567  # The port used by the server
 extension = "png"
 image_name = "pythonimage."+extension
+
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#condition = True #Receives Image
 client_socket.connect((HOST, PORT))
+orig_img = cv2.imread('documents/9.png')
+    # convert to grayscale to make the image single channel
+        
+        
+image1 = cv2.cvtColor(orig_img, cv2.COLOR_BGR2GRAY)
+image1 = cv2.resize(image1,(224,224))
+
+cv2.imwrite(f"documents/9.png", image1)
 
 with open('documents/9.png', 'rb') as file:
+    
+    # add one extra batch dimension
+    
     file_data = file.read(BUFFER_SIZE)
     while file_data:
         client_socket.send(file_data)
--- echo-server.py
+++ echo-server.py
@@ -1,39 +1,24 @@
 # echo-server.py
 import time
-import sys
 import socket
 import io
+import numpy as np
+from PIL import Image
+import torch
 import VGG11
 import torchvision.transforms as transforms
-import cv2
-#import glob as glob
-import numpy as np
-import torch
-from PIL import Image
-
-# simple image transforms
-transform = transforms.Compose([
-    transforms.ToPILImage(),
-    transforms.Resize((224, 224)),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.5],
-                         std=[0.5])
-])
 
 extension = "png"
 image_name = "pythonimage."+extension
-
     
 #HOST = "192.168.1.154"  # Standard loopback interface address (localhost)
 HOST = "127.0.0.1" #Localhost
 PORT = 4567  # Port to listen on (non-privileged ports are > 1023)
 BUFFER_SIZE = 4096
 
-
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((HOST, PORT))
 server.listen()
-
 
 client, addr = server.accept()
 file_stream = io.BytesIO()
@@ -46,60 +31,45 @@ while recv_data:
     if recv_data == b"%IMAGE_COMPLETED%":
           break
 
-image = Image.open(file_stream)
+image2 = Image.open(file_stream)
 #image = image.filter(ImageFilter.GaussianBlur(radius=10)) Line caused AI error
-image.save('clientfiles/'+image_name, format = 'PNG')
+image2.save('clientfiles/'+image_name, format = 'PNG')
 
 end = time.time() 
 
 print("Total time of initial data retrieval: " + str(end-start)+" seconds.")
 
-
 start1 = time.time()
-# inferencing on CPU
-device = 'cpu'
+
 # initialize the VGG11 model
 model = VGG11.VGG11(in_channels=1, num_classes=10)
 # load the model checkpoint
 checkpoint = torch.load('model.pth')
 # load the trained weights
 model.load_state_dict(checkpoint['model_state_dict'])
-model.to(device)
+
 model.eval()
 start3 = time.time()
-orig_img = cv2.imread('clientfiles/pythonimage.png')
-    # convert to grayscale to make the image single channel
-        
-        
-image1 = cv2.cvtColor(orig_img, cv2.COLOR_BGR2GRAY)
-image1 = transform(image1)
-    # add one extra batch dimension
-image1 = image1.unsqueeze(0).to(device)
+
+transform = transforms.ToTensor()
+tensor = transform(image2)
+tensor = tensor.unsqueeze_(0)
+print("Shape of tensor: " + str(tensor.shape))
+
 end = time.time()
 print("Model pre-processing time: " + str(end-start3)+ " seconds.")
     # forward pass the image through the model
 start4 = time.time()
-outputs = model(image1)
+
+outputs = model(tensor)
 end = time.time()
 print("Model processing time: " + str(end-start4)+ " seconds.")
-#print("Model processing time: " + str(end-start1)+ " seconds.")
-    # get the index of the highest score
-    # the highest scoring indicates the label for the Digit MNIST dataset
-label = np.array(outputs.detach()).argmax()
-original_stdout = sys.stdout # Save a reference to the original standard output
+label = np.array(outputs.detach()).argmax() # get the index of the highest score
+
 start2 = time.time()
 with open('clientfiles/output.txt', 'w') as file:
-    sys.stdout = file
-    print(f"{'pythonimage'}: {label}")
-    sys.stdout = original_stdout
+    file.write(f"{'pythonimage'}: {label}")
 file.close()
-    # put the predicted label on the original image
-#cv2.putText(orig_img, str(label), (15, 50), cv2.FONT_HERSHEY_SIMPLEX, 
-              #  2, (0, 255, 0), 2)
-    # show and save the resutls
-#@cv2.imshow('Result', orig_img)
-#cv2.waitKey(0)
-#cv2.imwrite(f"clientfiles/edited.png", orig_img)
 
 with open('clientfiles/output.txt', 'rb') as file:
     file_data = file.read(BUFFER_SIZE)
@@ -109,13 +79,6 @@ with open('clientfiles/output.txt', 'rb') as file:
 client.send(b"%IMAGE_COMPLETED%")
 file.close()
 
-    #with conn:
-     #   print(f"Connected by {addr}")
-      #  while True:
-         #   data = conn.recv(1024)
-          #  if not data:
-           #     break
-          #  conn.sendall(data)
 client.close()
 server.close()
 end = time.time()
